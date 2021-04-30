cadc94c1dc22a1969b92a7b010bf005b3865145a
nikos <nikos.evo@hotmail.com>
2021-04-30 22:18:24 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Contact from"./components/Contact";
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <Contact />
+    </div>
   )
   
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,33 +2,37 @@ import React from 'react'
 
 const Navbar = () => {
     return (
+        <div>
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
 
-                <a className="navbar-brand" href="#">Ev0luti0n</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                    <a className="navbar-brand" href="#">Ev0luti0n</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">About me <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Services <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Contact</a>
-                    </li>
-                    </ul>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">About me <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Services <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+
+            {/*contact form starts here*/}
+        </div>
 
     )
 }
