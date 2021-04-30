bea85a4dccc22073a0df66e78fd72f5a8d7ff1f5
nikos <nikos.evo@hotmail.com>
2021-04-30 22:47:09 +0200

    form

---
+++ src/components/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <br></br>
+            <br></br>
+            <br></br>
+            <form>
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+                </div>
+                <div className="form-group">
+                    <label>Name</label>
+                    <input type="name" className="form-control" id="exampleFormControlInput1" placeholder="name"></input>
+                </div>
+
+                <div className="form-group">
+                    <label>Example textarea</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+            </form>
+            
+        </div>
+    )
+}
+
+export default Contact
